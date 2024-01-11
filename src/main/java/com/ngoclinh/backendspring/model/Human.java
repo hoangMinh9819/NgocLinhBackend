c0e3f977087063f84ffb34cf826098d82e7ac69c
oscar <mynameisminh191998@gmail.com>
2024-01-11 14:59:57 +0700

    update gitignore on website gitignore.io with keyword Java Maven Intellij

--- src/main/java/com/ngoclinh/backendspring/model/Human.java
+++ src/main/java/com/ngoclinh/backendspring/model/Human.java
@@ -1,24 +1,18 @@
 package com.ngoclinh.backendspring.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name="tbl_humans")
-public class Human implements UserDetails {
+public class Human {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="human_id")
@@ -49,35 +43,5 @@ public class Human implements UserDetails {
     @Column(name="UPDATED_DATE")
     private LocalDateTime updatedDate;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-    @Override
-    public String getUsername(){
-        return username;
-    }
-    @Override
-    public String getPassword(){
-        return password;
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
